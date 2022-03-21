4b4e102ff0a11682188824118bd239c070834744
Moti Elmakies <motiphone2003@gmail.com>
2022-03-22 00:00:59 +0200

    added website part of the website icons and making tailwind finally working

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,9 @@ import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className='container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal'>
-      Hello world
+    <div className='text-blue-500'>
+      Hello
+      <h1>Hello</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FFDFD6',
+        test: 'red',
+      },
+    },
   },
   plugins: [],
 };
