0e3dcd22dfa02635ca197394e58bd18a9f7d96ad
Moti Elmakies <motiphone2003@gmail.com>
2022-03-25 10:15:47 +0300

    :bug: another icon bug

--- components/Input/Input.js
+++ components/Input/Input.js
@@ -5,7 +5,7 @@ const Input = ({ placeholder = '' }) => {
   return (
     <div className='flex items-center gap-1 border-2   border-gray-100  focus:border-primary-dark rounded-full p-1'>
       <Image
-        src='/images/icons/search.svg'
+        src='/images/icons/Search.svg'
         width='24px'
         height='24px'
         alt='Search Icon'
