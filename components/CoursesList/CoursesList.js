be3dce137a134d12796e9d73745cc4884dbf90c7
Moti Elmakies <motiphone2003@gmail.com>
2022-03-26 10:08:20 +0300

    :apple: some final touch

--- components/CoursesList/CoursesList.js
+++ components/CoursesList/CoursesList.js
@@ -6,36 +6,39 @@ import CoursesListItem from './CoursesListItem';
 // options - icon - windows.svg;
 const CoursesList = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <div className='flex gap-4 mb-6'>
-        <CoursesListMenuItem text={'New'} icon={'options-icon-windows.svg'} />
-        <CoursesListMenuItem text='Popular' icon={'options-icon-star.svg'} />
-        <CoursesListMenuItem text='Free' icon={'options-icon-heart.svg'} />
-        <CoursesListMenuItem text='Pro' icon={'options-icon-pro.svg'} />
+    <>
+      <h1 className='pl-8 py-10'>Courses List 😉</h1>
+      <div className='flex flex-col justify-center items-center'>
+        <div className='flex gap-4 mb-6'>
+          <CoursesListMenuItem text={'New'} icon={'options-icon-windows.svg'} />
+          <CoursesListMenuItem text='Popular' icon={'options-icon-star.svg'} />
+          <CoursesListMenuItem text='Free' icon={'options-icon-heart.svg'} />
+          <CoursesListMenuItem text='Pro' icon={'options-icon-pro.svg'} />
+        </div>
+        <div className='flex flex-wrap gap-4 justify-center'>
+          <CoursesListItem
+            header={'JavaScript'}
+            paragraph={'Advanced web applications.'}
+            length={'- 0 h 55 min'}
+          />
+          <CoursesListItem
+            header={'PowerPoint'}
+            paragraph={'Enhance your presentation skills.'}
+            length={'- 3 h 15 min'}
+          />
+          <CoursesListItem
+            header={'Data Science'}
+            paragraph={'Analyze the data like a pro.'}
+            length={'- 3 h 15 min'}
+          />
+          <CoursesListItem
+            header={'Digital Design'}
+            paragraph={'Learn the basics of the design.'}
+            length={'- 3 h 15 min'}
+          />
+        </div>
       </div>
-      <div className='flex flex-wrap gap-4 justify-center'>
-        <CoursesListItem
-          header={'JavaScript'}
-          paragraph={'Advanced web applications.'}
-          length={'- 0 h 55 min'}
-        />
-        <CoursesListItem
-          header={'PowerPoint'}
-          paragraph={'Enhance your presentation skills.'}
-          length={'- 3 h 15 min'}
-        />
-        <CoursesListItem
-          header={'Data Science'}
-          paragraph={'Analyze the data like a pro.'}
-          length={'- 3 h 15 min'}
-        />
-        <CoursesListItem
-          header={'Digital Design'}
-          paragraph={'Learn the basics of the design.'}
-          length={'- 3 h 15 min'}
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
